eed77910d86d321da1971988aa9ec43b65d5323f
Brian Gibson <bggibson@hotmail.com>
2020-04-22 14:51:49 -0400

    ES6 Classes to Construct Objects

--- DesignPatterns/ConstructorPattern/ConstructorPattern.js
+++ DesignPatterns/ConstructorPattern/ConstructorPattern.js
@@ -1,49 +1,36 @@
-function Student(name, university) {
+class StudentClass {
 
-    this.name = name;
-    this.university = university;
+    constructor(name, university, gpa) {
 
-    this.printDetails = function() {
-        console.log("***** printDetails");
+        this.name = name;
+        this.university = university;
+        this.gpa = gpa;
+
+        this.printDetails = function() {
+            console.log("***** printDetails");
+            console.log("Name: ", this.name,
+                "University: ", this.university,
+                "GPA: ", this.gpa);
+            console.log("*****");
+        }
+
+    }
+
+    showDetails() {
+        console.log("***** showDetails");
         console.log("Name: ", this.name,
             "University: ", this.university,
             "GPA: ", this.gpa);
         console.log("*****");
     }
+
 }
 
-var student_1 = new Student("Nora", "Pinnacle University");
 
-console.log("student_1: ", student_1);
+var student_10 = new StudentClass("Robert", "Professional University", 3.9);
 
-student_1.printDetails();
+console.log("student_10 : ", student_10);
 
-// TODO recording: Show on console, expand object, and show the new printDetails output
+student_10.printDetails();
 
-student_1.gpa = 3.7;
-
-console.log("student_1 with gpa: ", student_1);
-
-student_1.printDetails();
-
-
-Student.prototype.showDetails = function() {
-    console.log("***** showDetails");
-    console.log("Name: ", this.name,
-        "University: ", this.university,
-        "GPA: ", this.gpa);
-    console.log("*****");
-};
-
-
-student_1.showDetails();
-
-
-var student_2 = new Student("Damien", "Science University");
-student_2.gpa = 3.2;
-
-console.log("student_2 with gpa: ", student_2);
-
-student_2.printDetails();
-
-student_2.showDetails();
+student_10.showDetails();
