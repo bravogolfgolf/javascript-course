3331ecdd1e1627efcb90d9890bc471e0f8b1e125
Brian Gibson <bggibson@hotmail.com>
2020-04-22 14:20:54 -0400

    Assigning Properties and Functions

--- DesignPatterns/ConstructorPattern/ConstructorPattern.js
+++ DesignPatterns/ConstructorPattern/ConstructorPattern.js
@@ -1,3 +1,25 @@
-let student = {};
+var student = {};
 
 console.log("Empty student object: ", student);
+
+
+student.name = "Alice";
+student["university"] = "Amazing University";
+student.gpa = 3.8;
+
+console.log("student object with properties: ", student);
+
+
+
+student.printDetails = function() {
+    console.log("***** printDetails");
+    console.log("Name: ", this.name,
+        "University: ", this.university,
+        "GPA: ", this["gpa"]);
+    console.log("*****");
+}
+
+console.log("student object with function properties: ", student);
+
+
+student.printDetails();
